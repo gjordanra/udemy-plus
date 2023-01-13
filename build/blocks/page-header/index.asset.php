bfad7ad857af15e558c107f8e280810d3a9f1878
Gustavo Jordán Ramírez <gjordanrad@gmail.com>
2023-01-13 00:11:44 -0500

    added header category custom block

--- build/blocks/page-header/index.asset.php
+++ build/blocks/page-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'ce50fb66f83fddb2ad9e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '897b0adc92d09de4cb59');
